c7c29bc212a6427f897b8736381d838f4ffacdb9
Noah Ramey and Sebastian Prokuski <noah@>
2016-08-03 16:31:00 -0700

    progress

--- build/js/app.js
+++ build/js/app.js
@@ -36,6 +36,9 @@ SC.initialize({
 
 var randomArray = [];
 var random;
+var tracksUrlArray = [];
+var tracksArtArray = [];
+
 var findTrack = function() {
   random = (Math.floor((Math.random() * 300000000) + 10000000));
   randomArray.push(random);
@@ -57,27 +60,48 @@ var findTrack = function() {
   });
 };
 
+var findTracksForArray = function() {
+  random = (Math.floor((Math.random() * 300000000) + 10000000));
+  SC.get('/tracks/' + random).then(function(track){
+    tracksUrlArray.push(track.permalink_url);
+    if (track.artwork_url) {
+      tracksArtArray.push(track.artwork_url);
+    } else {
+      tracksArtArray.push(track.user.avatar_url);
+    }
+    console.log(tracksUrlArray);
+    console.log(tracksArtArray);
+  }).catch(function(error) {
+    findTracksForArray();
+  });
+};
+
 
 $(document).ready(function() {
-  findTrack();
-  $('#skip-song').click(function() {
-    $('#widget').removeClass("twelve columns").addClass("ten columns");
-    // $('#finding').show();
-    $('#skip-song').attr('disabled', true);
-    $('#last-songs').text("");
-    for (var i = randomArray.length; i > randomArray.length - 5; i--) {
-      if (randomArray[i]) {
-        SC.get("/tracks/" + randomArray[i]).then(function(track) {
-          if (track.artwork_url) {
-            $('#last-songs').prepend('<div class="row"><a target="_blank" href="' + track.permalink_url + '"><img src="' + track.artwork_url + '" class="thumb"></a></div>');
-          } else {
-            $('#last-songs').prepend('<div class="row"><a target="_blank" href="' + track.permalink_url + '"><img src="' + track.user.avatar_url + '" class="thumb"></a></div>');
-          }
-        });
-      }
-    }
-    findTrack();
-  });
+  for (var i = 0; i < 10; i++) {
+    findTracksForArray();
+  }
+
+  // }
+  // findTrack();
+  // $('#skip-song').click(function() {
+  //   $('#widget').removeClass("twelve columns").addClass("ten columns");
+  //   // $('#finding').show();
+  //   $('#skip-song').attr('disabled', true);
+  //   $('#last-songs').text("");
+  //   for (var i = randomArray.length; i > randomArray.length - 5; i--) {
+  //     if (randomArray[i]) {
+  //       SC.get("/tracks/" + randomArray[i]).then(function(track) {
+  //         if (track.artwork_url) {
+  //           $('#last-songs').prepend('<div class="row"><a target="_blank" href="' + track.permalink_url + '"><img src="' + track.artwork_url + '" class="thumb"></a></div>');
+  //         } else {
+  //           $('#last-songs').prepend('<div class="row"><a target="_blank" href="' + track.permalink_url + '"><img src="' + track.user.avatar_url + '" class="thumb"></a></div>');
+  //         }
+  //       });
+  //     }
+  //   }
+  //   findTrack();
+  // });
 });
 
 },{"soundcloud":1}]},{},[2]);
