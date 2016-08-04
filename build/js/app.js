2ce4e4f1cd44f5b5ffb0942fde1e91ed07aa8d95
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-08-03 19:09:45 -0700

    add features back in

--- build/js/app.js
+++ build/js/app.js
@@ -34,74 +34,56 @@ SC.initialize({
   client_id: '8418c9c411768d55ad2b573b274b1de8'
 });
 
-var randomArray = [];
 var random;
 var tracksUrlArray = [];
 var tracksArtArray = [];
+var playedTracksUrlArray = [];
+var playedTracksArtArray = [];
 
-var findTrack = function() {
-  random = (Math.floor((Math.random() * 300000000) + 10000000));
-  randomArray.push(random);
-  SC.get('/tracks/' + random).then(function(track){
-    if (track.playback_count > 0) {
-      SC.oEmbed(track.permalink_url, { auto_play: true }).then(function(result) {
-        $('#widget').html(result.html);
-        $('#skip-song').attr('disabled', false);
-        // $('#finding').hide();
-      });
-      console.log(track);
-    } else {
-      randomArray.pop();
-      findTrack();
-    }
-  }).catch(function(error) {
-    randomArray.pop();
-    findTrack();
-  });
-};
 
 var findTracksForArray = function() {
   random = (Math.floor((Math.random() * 300000000) + 10000000));
   SC.get('/tracks/' + random).then(function(track){
-    tracksUrlArray.push(track.permalink_url);
-    if (track.artwork_url) {
-      tracksArtArray.push(track.artwork_url);
+    if (track.playback_count > 100) {
+      tracksUrlArray.push(track.permalink_url);
+      if (track.artwork_url) {
+        tracksArtArray.push(track.artwork_url);
+      } else {
+        tracksArtArray.push(track.user.avatar_url);
+      }
     } else {
-      tracksArtArray.push(track.user.avatar_url);
+      findTracksForArray();
     }
-    console.log(tracksUrlArray);
-    console.log(tracksArtArray);
   }).catch(function(error) {
     findTracksForArray();
   });
 };
 
+var counter = 0;
+var loadTrack = function() {
+  SC.oEmbed(tracksUrlArray[counter], { auto_play: true }).then(function(result) {
+    $('#widget').html(result.html);
+  });
 
+  // $('#last-songs').prepend('<div class="row"><a target="_blank" href="' + playedTracksUrlArray[counter - 1] + '"><img src="' + playedTracksArtArray[counter - 1] + '" class="thumb"></a></div>');
+
+  playedTracksUrlArray.push(tracksUrlArray[counter]);
+  playedTracksArtArray.push(tracksArtArray[counter]);
+  counter += 1;
+};
+
+
+// FRONTEND
 $(document).ready(function() {
   for (var i = 0; i < 10; i++) {
     findTracksForArray();
   }
 
-  // }
-  // findTrack();
-  // $('#skip-song').click(function() {
-  //   $('#widget').removeClass("twelve columns").addClass("ten columns");
-  //   // $('#finding').show();
-  //   $('#skip-song').attr('disabled', true);
-  //   $('#last-songs').text("");
-  //   for (var i = randomArray.length; i > randomArray.length - 5; i--) {
-  //     if (randomArray[i]) {
-  //       SC.get("/tracks/" + randomArray[i]).then(function(track) {
-  //         if (track.artwork_url) {
-  //           $('#last-songs').prepend('<div class="row"><a target="_blank" href="' + track.permalink_url + '"><img src="' + track.artwork_url + '" class="thumb"></a></div>');
-  //         } else {
-  //           $('#last-songs').prepend('<div class="row"><a target="_blank" href="' + track.permalink_url + '"><img src="' + track.user.avatar_url + '" class="thumb"></a></div>');
-  //         }
-  //       });
-  //     }
-  //   }
-  //   findTrack();
-  // });
+  $('#skip-song').click(function() {
+    $('#widget').removeClass("twelve columns").addClass("ten columns");
+    $('#last-songs').text("");
+    loadTrack(counter);
+  });
 });
 
 },{"soundcloud":1}]},{},[2]);
