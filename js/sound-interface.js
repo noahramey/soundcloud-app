8fba01c3dc13e4ce2ac518c5bcc1e274bf8845db
Noah Ramey and Sebastian Prokuski <sebastian@>
2016-08-04 14:28:14 -0700

    refactor javascript files and add object constructor for playlist

--- js/sound-interface.js
+++ js/sound-interface.js
@@ -1,64 +1,19 @@
-var SC = require('soundcloud');
+var Playlist = require('./../js/sound.js').playlistModule;
 
-SC.initialize({
-  client_id: '8418c9c411768d55ad2b573b274b1de8'
-});
-
-var random;
-var tracksUrlArray = [];
-var tracksArtArray = [];
-var playedTracksUrlArray = [];
-var playedTracksArtArray = [];
-
-
-var findTracksForArray = function() {
-  random = (Math.floor((Math.random() * 300000000) + 10000000));
-  SC.get('/tracks/' + random).then(function(track){
-    if (track.playback_count > 500) {
-      tracksUrlArray.push(track.permalink_url);
-      if (track.artwork_url) {
-        tracksArtArray.push(track.artwork_url);
-      } else {
-        tracksArtArray.push(track.user.avatar_url);
-      }
-    } else {
-      findTracksForArray();
-    }
-  }).catch(function(error) {
-    findTracksForArray();
-  });
-};
-
-var counter = 0;
-var loadTrack = function() {
-  SC.oEmbed(tracksUrlArray[counter], { auto_play: true }).then(function(result) {
-    $('#widget').html(result.html);
-  });
-
-  for (var i = 0; i < playedTracksUrlArray.length; i++) {
-    $('#last-songs').prepend('<div class="row"><a target="_blank" href="' + playedTracksUrlArray[i] + '"><img src="' + playedTracksArtArray[i] + '" class="thumb"></a></div>');
-  }
-
-  playedTracksUrlArray.push(tracksUrlArray[counter]);
-  playedTracksArtArray.push(tracksArtArray[counter]);
-  counter += 1;
-};
-
-
-// FRONTEND
 $(document).ready(function() {
+  var myPlaylist = new Playlist();
   for (var i = 0; i < 50; i++) {
-    findTracksForArray();
+    myPlaylist.findTracksForArray();
   }
   $('#start-stream').click(function() {
     $('#start-stream').hide();
     $('#skip-song').show();
     $('#widget').fadeIn();
-    loadTrack();
+    myPlaylist.loadTrack();
   });
   $('#skip-song').click(function() {
     $('#widget').removeClass("twelve columns").addClass("ten columns");
     $('#last-songs').text("");
-    loadTrack();
+    myPlaylist.loadTrack();
   });
 });
--- js/sound.js
+++ js/sound.js
@@ -1,11 +1,50 @@
-// var SC = require('soundcloud');
-//
-// SC.initialize({
-//   client_id: '8418c9c411768d55ad2b573b274b1de8'
-// });
+var SC = require('soundcloud');
 
-var randomizer = function() {
-  Math.floor((Math.random() * 300000000) + 10000000);
+SC.initialize({
+  client_id: '8418c9c411768d55ad2b573b274b1de8'
+});
+
+function Playlist() {
+  this.tracksUrlArray = [];
+  this.tracksArtArray = [];
+  this.playedTracksUrlArray = [];
+  this.playedTracksArtArray = [];
+  this.counter = 0;
+}
+
+Playlist.prototype.findTracksForArray = function() {
+  var self = this;
+  var random = (Math.floor((Math.random() * 300000000) + 10000000));
+  SC.get('/tracks/' + random).then(function(track){
+    if (track.playback_count > 0) {
+      self.tracksUrlArray.push(track.permalink_url);
+      if (track.artwork_url) {
+        self.tracksArtArray.push(track.artwork_url);
+      } else {
+        self.tracksArtArray.push(track.user.avatar_url);
+      }
+    } else {
+      self.findTracksForArray();
+    }
+  }).catch(function(error) {
+    self.findTracksForArray();
+  });
 };
 
-// exports.soundModule = SC;
+Playlist.prototype.loadTrack = function() {
+  var self = this;
+  SC.oEmbed(self.tracksUrlArray[self.counter], { auto_play: true }).then(function(result) {
+    $('#widget').html(result.html);
+  });
+
+  for (var i = 0; i < self.playedTracksUrlArray.length; i++) {
+    $('#last-songs').prepend('<div class="row"><a target="_blank" href="' + self.playedTracksUrlArray[i] + '"><img src="' + self.playedTracksArtArray[i] + '" class="thumb"></a></div>');
+  }
+
+  self.playedTracksUrlArray.push(self.tracksUrlArray[self.counter]);
+  self.playedTracksArtArray.push(self.tracksArtArray[self.counter]);
+  self.counter += 1;
+};
+
+exports.soundCloudModule = SC;
+exports.playlistModule = Playlist;
